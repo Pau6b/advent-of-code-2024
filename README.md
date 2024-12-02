ef3930e1407c6e38d6bfc4eee886fc530852a8cd
Pau Murciano Julià <pau.murciano@gameloft.com>
2024-12-02 16:01:37 +0100

    + Day 2

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Trying to get better at other languages.
 |        | Problem                                            | Solution                                                                       | Finished |
 |--------|----------------------------------------------------|--------------------------------------------------------------------------------|----------|
 | Day 1  | [Problem 1](https://adventofcode.com/2024/day/1)   | [day01.rs](https://github.com/Pau6b/advent-of-code-2024/blob/main/src/day01.rs)| ✓        |
+| Day 2  | [Problem 2](https://adventofcode.com/2024/day/2)   | [day01.rs](https://github.com/Pau6b/advent-of-code-2024/blob/main/src/day02.rs)| ✓        |
---
+++ src/day02.rs
@@ -0,0 +1,53 @@
+use std::{fs::File, io::Read};
+use itertools::Itertools;
+
+fn is_combination_safe(curr : &i32, next : &i32, is_ascendent : bool) -> bool {
+    !((is_ascendent && curr >= next) || (!is_ascendent && curr <= next) || (curr-next).abs() > 3)
+}
+
+fn is_seq_safe(sequence: &Vec<i32>) -> bool {
+    let is_ascendent = sequence[0] < sequence[1];
+    for (curr, next) in sequence.into_iter().tuple_windows() {
+        if !is_combination_safe(curr, next, is_ascendent) {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_seq_safe_skipping(sequence: &Vec<i32>, pos_to_skip: usize) -> bool {
+    let skipped_sequence: Vec<i32> = sequence.into_iter().enumerate().filter(|(i, _)| *i != pos_to_skip).map(|(_, v)| *v).collect();
+    is_seq_safe(&skipped_sequence)
+}
+
+pub fn problem1() {
+    let mut file = File::open("input/day2.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let mut safe_reports = 0;
+    for line in contents.lines() {
+        let parsed_input : Vec<i32> = line.split(" ").map(|input| {input.parse().unwrap()}).collect();
+        if is_seq_safe(&parsed_input)
+        {
+            safe_reports += 1;
+        }
+    }
+    println!("[{}]", safe_reports)
+}
+
+pub fn problem2() {
+    let mut file = File::open("input/day2.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let mut safe_reports = 0;
+    for line in contents.lines() {
+        let parsed_input : Vec<i32> = line.split(" ").map(|input| {input.parse().unwrap()}).collect();
+        let is_safe =  is_seq_safe(&parsed_input) ||
+                             (0..parsed_input.len()).any(|pos| is_seq_safe_skipping(&parsed_input, pos));
+        if is_safe
+        {
+            safe_reports += 1;
+        }
+    }
+    println!("[{}]", safe_reports)
+}
